dd04a720f9d74d3a1483185e09fa5dcf97e7ae00
Felipe Augusto <felipe.augusto.1303@gmail.com>
2021-05-31 13:04:22 -0400

    First commit
    Signed-off-by: Felipe Augusto <felipe.augusto.1303@gmail.com>

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Card.css';
+
+const Card = ({value}) => {
+    const [flip, setFlip] = React.useState(false)
+
+    const onFlip = () => {
+        console.log("clique!")
+        setFlip(!flip)
+    }
+
+    return(
+        <div className="card-container">
+            <div className={flip ? "card-flipped":"card-not-flipped"} 
+                onClick={() => onFlip()}>
+                    <p className={flip? "hidden-text":"show-text"}>{value}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
