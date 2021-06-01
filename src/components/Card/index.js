f263b94a510f47bc293aab97e23f8fca222c2acd
Felipe Augusto <felipe.augusto.1303@gmail.com>
2021-05-31 20:37:32 -0400

    implemented game rules
    Signed-off-by: Felipe Augusto <felipe.augusto.1303@gmail.com>

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Card.css';
 
-const Card = ({value, isFlip, onClick}) => {
+const Card = ({value, isFlip, onClick, done, disable}) => {
 
     return(
         <div className="card-container">
-            <div className={!isFlip ? "card-flipped":"card-not-flipped"} 
-                onClick={onClick}>
+            {done && <div className={!isFlip ? "card-flipped":"card-not-flipped"} 
+                onClick={onClick}
+                disable={disable}>
                     <p className={!isFlip? "hidden-text":"show-text"}>{value}</p>
-            </div>
+            </div>}
         </div>
     )
 }
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,11 +4,42 @@ import './Game.css';
 
 const Game = ({ nElements }) => {
     const [values, setValues] = React.useState([]);
-    const [flag, setFlag] = React.useState(false)
+    const [initValue, setInitValue] = React.useState([])
+    const [flag, setFlag] = React.useState(false);
+    const [selectedCards, setSelectedCards] = React.useState([])
+    const [disable, setDisable] = React.useState(false)
+
 
     React.useEffect(()=>{
-        setValues(generateArray(nElements))
+        const generated = generateArray(nElements)
+        setValues(generated)
+        setInitValue(generated)
     },[])
+
+    React.useEffect(()=>{
+        if(disable){
+            const selectedValues = values.filter((element) => element.flipped === true)
+            if(selectedValues[0].value === selectedValues[1].value){
+                setTimeout(()=>{
+                    values[selectedValues[0].index].visible = false
+                    values[selectedValues[1].index].visible = false
+                    values[selectedValues[0].index].flipped = false
+                    values[selectedValues[1].index].flipped = false
+                    setValues(values)
+                    setDisable(false)
+                }, 2000)
+            }
+            else{
+                setTimeout(()=>{
+                    values[selectedValues[0].index].flipped = false
+                    values[selectedValues[1].index].flipped = false
+                    setValues(values)
+                    setDisable(false)
+                }, 2000)
+                
+            }
+        }
+    },[disable])
 
     const generateArray = (nElements) => {
         const values = []
@@ -25,18 +56,26 @@ const Game = ({ nElements }) => {
         values.sort(() => Math.random() - 0.5).forEach((element, idx) => {
             jsonValues.push({
                 "value": element,
-                "flipped": false
+                "index": idx,
+                "flipped": false,
+                "visible": true
             })
         })
         return jsonValues
     }
 
 
-    const onFlip = (values,idx) => {
-        console.log("clique!")
-        values[idx].flipped = !values[idx].flipped
-        setValues(values)
-        setFlag(!flag)
+    const onFlip = (values,idx,disable) => {
+        if(!disable){
+            console.log("clique!")
+            values[idx].flipped = !values[idx].flipped
+            setValues(values)
+            setFlag(!flag)
+            if(values.filter((element)=> element.flipped === true).length > 1){
+                console.log("entrei")
+                setDisable(true)
+            }
+        }
     }
 
     const gameRules = () => {}
@@ -46,7 +85,11 @@ const Game = ({ nElements }) => {
             {values.map((item, idx) => {
                 return (
                     values && <div key={idx}>
-                        <Card value={item.value} isFlip={item.flipped} onClick={() => onFlip(values,idx)}/>
+                        <Card 
+                            value={item.value} 
+                            isFlip={item.flipped} 
+                            onClick={() => onFlip(values,idx, disable)} 
+                            done={item.visible}/>
                     </div>
                 )
             })}
